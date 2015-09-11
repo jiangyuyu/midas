a990f80e0d9c54718bb2d33f10fbc64129db708b
Xian Li <xli1@linkedin.com>
2015-09-11 11:48:18 -0700

    algorithm problems

--- algorithm/src/main/java/com/midas/algorithm/DP.java
+++ algorithm/src/main/java/com/midas/algorithm/DP.java
@@ -1,8 +1,5 @@
 package com.midas.algorithm;
 
-import java.util.HashSet;
-
-
 /**
  * Created by xli1 on 4/9/15.
  */
@@ -27,14 +24,59 @@ public class DP {
     return maxProfit;
   }
 
+  // too much time and space
+  public int numSquares(int n) {
+    int[][] f = new int[n+1][n+1];
+
+    for (int i = 1; i <= n; i++) {
+      f[1][i] = i;
+    }
+    for (int i = 2; i <= n; i++) {
+      for (int j = i; j <= n; j++) {
+        if (j > i * i) {
+          f[i][j] = Math.min(f[i - 1][j], 1 + f[i][j - i * i]);
+        } else if (j == i * i) {
+          f[i][j] = 1;
+        } else {
+          f[i][j] = f[i-1][j];
+        }
+      }
+    }
+
+    return f[n][n];
+  }
+  public int numSquares2(int n) {
+    if (n == 0) return 0;
+    int[] f = new int[n+1];
+    for (int i = 0; i * i <= n; i++) {
+      f[i*i] =  1;
+    }
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; i + j * j <=n; j++) {
+        if (f[i + j*j] == 0 || f[i + j*j] > f[i] + 1) {
+          f[i + j*j] = f[i] + 1;
+        }
+      }
+    }
+    return f[n];
+  }
+
+  public int numWaysPainting(int n, int k) {
+    if (n <= 1 || k == 0) return n * k;
+    int same = 0, diff = k;
+    for (int i = 1; i < n; i++) {
+      int curSame = diff;
+      int curDiff = (k-1)*(same + diff);
+      same = curSame;
+      diff = curDiff;
+    }
+    return same + diff;
+  }
+
   public static void main(String[] args) {
     int[] array = {5, 4, 5, 11, 5};
     DP dp = new DP();
-    System.out.println(dp.bestThief(array));
+    System.out.println(dp.numWaysPainting(4, 2));
 
-    HashSet<Character.UnicodeScript> set = new HashSet<Character.UnicodeScript>();
-    for (Character.UnicodeScript s : Character.UnicodeScript.values()) set.add(s);
-    System.out.println(set.size() + "\t");
-    System.out.println(set.size() + "\n");
   }
 }
---
+++ algorithm/src/main/java/com/midas/algorithm/Tree.java
@@ -0,0 +1,18 @@
+package com.midas.algorithm;
+
+/**
+ * Created by Xian Li  on 9/11/15.
+ */
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) {
+    val = x;
+  }
+}
+public class Tree {
+  public void inorderWithStack(TreeNode root) {
+
+  }
+}
