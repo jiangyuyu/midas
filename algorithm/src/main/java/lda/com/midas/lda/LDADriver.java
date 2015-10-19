06d2e7e7eec73ca1df4ae876ba0fb07a66e6402e
Xian Li <xli1@linkedin.com>
2015-10-19 09:17:55 -0700

    redo

--- algorithm/src/main/java/lda/com/midas/lda/LDADriver.java
+++ algorithm/src/main/java/lda/com/midas/lda/LDADriver.java
@@ -1,7 +1,7 @@
 package lda.com.midas.lda;
 
 /**
- * Created by xli1 on 8/24/15.
+ * Created by Xian Li (naixlee@gmail.com) on 8/24/15.
  */
 public class LDADriver {
   public static void main(String[] args) {
