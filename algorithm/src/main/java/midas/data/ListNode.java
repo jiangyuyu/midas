235e1a2f3100a7ac91ee83f38fb9962dc4791971
Xian Li <xli1@linkedin.com>
2016-02-12 16:27:33 -0800

    new exercise

---
+++ algorithm/src/main/java/midas/data/ListNode.java
@@ -0,0 +1,10 @@
+package midas.data;
+
+/**
+ * Created by xli1 on 2/11/16.
+ */
+public class ListNode {
+  int val;
+  ListNode next;
+}
+
---
+++ algorithm/src/main/java/midas/data/TreeNode.java
@@ -0,0 +1,10 @@
+package midas.data;
+
+/**
+ * Created by xli1 on 2/11/16.
+ */
+public class TreeNode {
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+}
